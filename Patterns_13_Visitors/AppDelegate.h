14aedf039e43697c6da15d8d2de1425a25b4ca3d
Ильяс <i.almakaev@gmail.com>
2017-08-11 18:53:45 +0700

    Initial Commit

---
+++ Patterns_13_Visitors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_13_Visitors
+//
+//  Created by Ильяс on 11.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Patterns_13_Visitors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Patterns_13_Visitors
+//
+//  Created by Ильяс on 11.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
